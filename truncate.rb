66e984ebc031e81524e2708b845bb82152724c8d
Miguel Beltrán Sanz <miguelbeltransanz@gmail.com>
2020-02-05 23:33:15 +0100

    extract error messages to its own class

--- truncate.rb
+++ truncate.rb
@@ -6,15 +6,6 @@ def truncate
     [ '--reference', '-r', GetoptLong::REQUIRED_ARGUMENT ]
   )
 
-  missing_size_or_reference_message = <<~EOF
-    truncate: you must specify either ‘--size’ or ‘--reference’
-    Try 'truncate --help' for more information.
-  EOF
-  missing_file_path_message = <<~EOF
-    truncate: missing file operand
-    Try 'truncate --help' for more information.
-  EOF
-
   size = nil
   reference = nil
 
@@ -29,29 +20,29 @@ def truncate
 
   path = ARGV.shift
 
-  if size
-    if path
-      file_content = File.read(path)
-
-      if size.to_i == 0
-        truncated_content_in_bytes = []
-      else
-        truncated_content_in_bytes = file_content.bytes[0..(size - 1)]
-      end
-
-      truncated_content = truncated_content_in_bytes.pack('c*')
-
-      File.open(path, 'w') do |file|
-        file.write(truncated_content)
-      end
-
-      Output.success
-    else
-      Output.failure(missing_file_path_message)
-    end
-  else
-    Output.failure(missing_size_or_reference_message)
+  if !size
+    return Output.failure(:missing_size_or_reference_message)
   end
+
+  if !path
+    return Output.failure(:missing_file_path_message)
+  end
+
+  file_content = File.read(path)
+
+  if size.to_i == 0
+    truncated_content_in_bytes = []
+  else
+    truncated_content_in_bytes = file_content.bytes[0..(size - 1)]
+  end
+
+  truncated_content = truncated_content_in_bytes.pack('c*')
+
+  File.open(path, 'w') do |file|
+    file.write(truncated_content)
+  end
+
+  Output.success
 end
 
 class Output
@@ -61,17 +52,36 @@ class Output
   attr_reader :message, :exit_code
 
   class << self
-    def failure(message)
+    def failure(message_identifier)
+      message = ErrorMessages.send(message_identifier)
       new(message, FAILURE_EXIT_CODE)
     end
 
-    def success(message=nil)
-      new(message, SUCCESS_EXIT_CODE)
+    def success
+      new(nil, SUCCESS_EXIT_CODE)
     end
   end
 
   def initialize(message, exit_code)
     @message = message
     @exit_code = exit_code
+  end
+end
+
+class ErrorMessages
+  class << self
+    def missing_size_or_reference_message
+      <<~EOF
+        truncate: you must specify either ‘--size’ or ‘--reference’
+        Try 'truncate --help' for more information.
+      EOF
+    end
+
+    def missing_file_path_message
+      <<~EOF
+        truncate: missing file operand
+        Try 'truncate --help' for more information.
+      EOF
+    end
   end
 end
